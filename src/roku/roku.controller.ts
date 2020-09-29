5baa224dfbfcdd8f9b173cc320a4632ed3bab5b2
Brandon Pina <brandonpi265@gmail.com>
2020-09-29 00:38:46 -0500

    remove roku module

--- src/roku/roku.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('roku')
-export class RokuController {}
--- src/roku/roku.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RokuController } from './roku.controller';
-import { RokuService } from './roku.service';
-
-@Module({
-  controllers: [RokuController],
-  providers: [RokuService]
-})
-export class RokuModule {}
