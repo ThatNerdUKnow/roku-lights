4e7c1f3acdc818538adb49fc56a06eed30bd68c4
Brandon Pina <brandonpi265@gmail.com>
2020-09-29 01:02:03 -0500

    poll roku player state

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RokuModule } from './roku/roku.module';
 import { HueModule } from './hue/hue.module';
 
 @Module({
-  imports: [RokuModule, HueModule],
+  imports: [HueModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/hue/hue.service.ts
+++ src/hue/hue.service.ts
@@ -1,45 +1,45 @@
-import { Injectable, HttpService, HttpException, HttpCode, HttpStatus } from '@nestjs/common';
-
+import {
+  Injectable,
+  HttpService,
+  HttpException,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Injectable()
 export class HueService {
-    
-
-   constructor(private http:HttpService)
-   {
+  constructor(private http: HttpService) {
     this.setColor(0);
-   }
+  }
 
-   private color;
-   private username;
+  private color;
+  private username;
 
-   setColor(hue:number)
-   {
-      if(hue <= 65535|| hue >=0)
-      {
-        this.color = hue;
-      }
-      else
-      {
-          throw new HttpException("Maximum value is 65535",HttpStatus.INTERNAL_SERVER_ERROR);
-      }
-   }
-
-    async discover()
-    {
-        // discovery.meethue.com
-        let ipRange;
-        await this.http.get("http://discovery.meethue.com").toPromise().then((response)=>{
-            ipRange = response.data[0];
-            
-        })
-        return ipRange
+  setColor(hue: number) {
+    if (hue <= 65535 || hue >= 0) {
+      this.color = hue;
+    } else {
+      throw new HttpException(
+        'Maximum value is 65535',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+  }
 
+  async discover() {
+    // discovery.meethue.com
+    let ipRange;
+    await this.http
+      .get('http://discovery.meethue.com')
+      .toPromise()
+      .then(response => {
+        ipRange = response.data[0];
+      });
+    return ipRange;
+  }
 
-    async lightsDown()
-    {
-        /*
+  async lightsDown() {
+    /*
         PUT
         http://192.168.0.167/api/ZVJgckeiew82iNZ8Ip1A-wkrbzObC7JQbsCHXMJw/groups/1/action
         {
@@ -48,44 +48,73 @@ export class HueService {
             "bri":254
         }
         */
-        await this.http.put("http://192.168.0.167/api/ZVJgckeiew82iNZ8Ip1A-wkrbzObC7JQbsCHXMJw/groups/1/action",{
-            "on":true,
-            "hue":0,
-            "bri":254,
-            "sat":254
-        }).toPromise().then((res)=>{
-            console.log(res.data);
-        });
-        return true;
+    await this.http
+      .put(
+        'http://192.168.0.167/api/ZVJgckeiew82iNZ8Ip1A-wkrbzObC7JQbsCHXMJw/groups/1/action',
+        {
+          on: true,
+          hue: 0,
+          bri: 254,
+          sat: 254,
+        },
+      )
+      .toPromise()
+      .then(res => {
+        console.log(res.data);
+      });
+    return true;
+  }
 
-    }
+  async lightsUp() {
+    await this.http
+      .put(
+        'http://192.168.0.167/api/ZVJgckeiew82iNZ8Ip1A-wkrbzObC7JQbsCHXMJw/groups/1/action',
+        {
+          on: true,
+          hue: 0,
+          bri: 254,
+          sat: 0,
+        },
+      )
+      .toPromise()
+      .then(res => {
+        console.log(res.data);
+      });
+    return true;
+  }
+  async register(ip: string) {
+    let body = { devicetype: 'roku-lights' };
+    let data;
+    await this.http
+      .post(`http://${ip}/api`, body)
+      .toPromise()
+      .then(response => {
+        try {
+          this.username = response.data[0].success.username;
+          console.log(this.username);
+        } catch (err) {
+          console.log(err);
+        }
+        data = response.data;
+      });
+    return data;
+  }
 
-    async lightsUp()
-    {
-        await this.http.put("http://192.168.0.167/api/ZVJgckeiew82iNZ8Ip1A-wkrbzObC7JQbsCHXMJw/groups/1/action",{
-            "on":true,
-            "hue":0,
-            "bri":254,
-            "sat":0
-        }).toPromise().then((res)=>{
-            console.log(res.data);
-        });
-        return true;
-    }
-    async register(ip:string)
-    {
-        let body = {"devicetype":"roku-lights"}
-        let data;
-        await this.http.post(`http://${ip}/api`,body).toPromise().then((response)=>{
-            try{
-            this.username = response.data[0].success.username
-            console.log(this.username);
-            }
-            catch(err){console.log(err)}
-            data = response.data;
+  pollRoku() {
+    setInterval(()=>{
+        this.http
+        .get('http://192.168.0.120:8060/query/media-player')
+        .toPromise()
+        .then(res => {
+          console.log(res.data);
         })
-        return data;
-    }
+    },5000);
+       
+      
+  }
 
-
+  onModuleInit() {
+    console.log('init');
+    this.pollRoku();
+  }
 }
