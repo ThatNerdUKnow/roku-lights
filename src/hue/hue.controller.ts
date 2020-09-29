d5bc3b9badc3023faf430a664e63b46e9c5cc896
Brandon Pina <brandonpi265@gmail.com>
2020-09-28 22:10:46 -0500

    Store hue username in memory

--- src/hue/hue.controller.ts
+++ src/hue/hue.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import {HueService} from './hue.service';
+import { stringify } from 'querystring';
 
 @Controller('hue')
-export class HueController {}
+export class HueController {
+
+    
+
+    constructor(private hueService:HueService)
+    {}
+
+    
+
+    @Get("/discover")
+    async discover()
+    {
+        console.log(await this.hueService.discover());
+        
+        return await this.hueService.discover();
+    }
+
+    @Get("/register")
+    async register(@Query('bridge')bridge:string)
+    {
+        
+        let data= await this.hueService.register(bridge);
+        return data;
+    }
+}
--- src/hue/hue.service.ts
+++ src/hue/hue.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+
 
 @Injectable()
-export class HueService {}
+export class HueService {
+    
+
+   constructor(private http:HttpService)
+   {
+
+   }
+
+   private username;
+
+    async discover()
+    {
+        // discovery.meethue.com
+        let ipRange;
+        await this.http.get("http://discovery.meethue.com").toPromise().then((response)=>{
+            ipRange = response.data[0];
+            
+        })
+        return ipRange
+    }
+
+    async register(ip:string)
+    {
+        let body = {"devicetype":"roku-lights"}
+        let data;
+        await this.http.post(`http://${ip}/api`,body).toPromise().then((response)=>{
+            try{
+            this.username = response.data[0].success.username
+            console.log(this.username);
+            }
+            catch(err){console.log(err)}
+            data = response.data;
+        })
+        return data;
+    }
+
+
+}
